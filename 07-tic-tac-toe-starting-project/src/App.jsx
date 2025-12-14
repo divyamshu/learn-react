0acd48bf3353442485f2664efbc5d87531e7455b
Divyamshu Gupta <divyamshuk@gmail.com>
2025-12-14 19:41:15 +0530

    75. Concept Repetition: Splitting Components & Building Reusable Components

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,16 @@
 
-function App() {
-  
+import Player from "./components/Player"
 
+function App() {
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />  {/* Each Player component works in isolation from each other */}
+        </ol>
+      </div>
+    </main>
   )
 }
 
